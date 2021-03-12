b277cdd344561fab8838d33f98c7206eef282ce1
Clifford Morin <cemorin21@gmail.com>
2021-03-12 14:27:28 -0500

    comments in js to help understand what each peice of code does exactly, udated read me with gif and site link

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Work-Day-Scheduler
+
+
+
+
+![Quiz Demo](images/demo.gif)
+
+[Site Link](https://cliffordmorin.github.io/Work-Day-Scheduler/)
+
+[![LinkedIn Badge](https://img.shields.io/badge/LinkedIn-Profile-informational?style=flat&logo=linkedin&logoColor=white&color=0D76A8)](https://www.linkedin.com/in/morin-clifford-129888a9/)
--- script.js
+++ script.js
@@ -4,33 +4,34 @@ $(document).ready(function() {
 
         //get textarea values from description class
         var value = $(this).siblings('.description').val()
-        //get id value from timeblock which we will convert into a number
+        //get id value from time block which we will convert into a number
         var time = $(this).parent().attr('id');
 
         // save values in local storage
         localStorage.setItem(time, value);
     });
 
-    //need to loop over the time blocks to check if the current hour is before, equal to, or after the timeblock hour
+    //loop over the time blocks to check if the current hour is before, equal to, or after the timeblock hour
     function hourTracker() {
         //get current hour ... moment().hours() counts hours in military time 0-23 so id for each block needs to be altered for that... hour-1 needs to be hour-13
         var currentHour = moment().hours()
         // console.log(currentHour);
-        //need to convert the .time-block into a number somehow change "hour-9" to "9" and put that into a variable (blockHour)
+        //convert the .time-block from "hour-9" to "9" and put that into a variable (blockHour). 
+        // parseInt converts string numbers into numbers. The attr grabs the id of .time-block then we split the 'hour-' and grab the 1st index which is the number.
         $('.time-block').each( function(){
            var blockHour = parseInt($(this).attr('id').split('hour-')[1])
         //    console.log(blockHour);
         //    console.log($(this).children('textarea'));
-           //if statement (blockHour < currentHour) add .past class to .time-block
+           //if statement (blockHour < currentHour) add .past class to the text area of .time-block
         if (blockHour < currentHour) {
             $(this).children('textarea').addClass('past');
         }
-            //else if (blockHour === currentHour) remove the .past class and add .present class to .time-block
+            //else if (blockHour === currentHour) remove the .past class and add .present class to the text area of .time-block
         else if (blockHour === currentHour) {
             $(this).children('textarea').removeClass('past');
             $(this).children('textarea').addClass('present');
         }
-        //else (blockHour > currentHour) remove .past and .present class and add .future class to .time-block
+        //else (blockHour > currentHour) remove .past and .present class and add .future class to the text area of .time-block
         else {
             $(this).children('textarea').removeClass('past');
             $(this).children('textarea').removeClass('present');
@@ -43,7 +44,7 @@ $(document).ready(function() {
     }
     hourTracker();
    
-    //set up interval to check if current time needs to updated
+    //Updates hour tracker function every second.
     setInterval(hourTracker,1000);
 
     //load saved data from local storage so it is still in description when you open up a new page
